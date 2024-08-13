a87c6a82d981f71222f10c9e70edd0a2ba631dca
Viacheslav Titenko <marokovzp1@gmail.com>
2024-08-13 23:32:30 +0300

    vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fmt/core.h>
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
 
 int main() {
 	std::cout << "Hello Bot" << std::endl;
+	fmt::print("Hello fmt!\n");
 
-	return 0;
+    // Виконати GET-запит до публічного API
+    cpr::Response r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/todos/1"});
+
+    // Вивести статус код відповіді
+    std::cout << "Status Code: " << r.status_code << std::endl;
+
+    // Парсити JSON відповідь
+    nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
+
+    // Вивести певне поле з JSON відповіді
+    std::cout << "Title: " << jsonResponse["title"] << std::endl;
+
+    return 0;
 }
