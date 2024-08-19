83141c2cd0733cb0ce0f04c0f2fc1ea426061b8f
Viacheslav Titenko <marokovzp1@gmail.com>
2024-08-19 19:50:32 +0300

    Commit name

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,17 @@ int main() {
 	fmt::print("Hello fmt!\n");
 
     // Виконати GET-запит до публічного API
-    cpr::Response r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/todos/1"});
+    cpr::Response r = cpr::Get(cpr::Url{"http://echo.jsontest.com/key/value/12one/t2wo"});
 
     // Вивести статус код відповіді
     std::cout << "Status Code: " << r.status_code << std::endl;
+    std::cout << "Full responce: " << r.text << std::endl;
 
     // Парсити JSON відповідь
     nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
 
     // Вивести певне поле з JSON відповіді
-    std::cout << "Title: " << jsonResponse["title"] << std::endl;
+    std::cout << "Title: " << jsonResponse["12one"] << std::endl;
 
     return 0;
 }
